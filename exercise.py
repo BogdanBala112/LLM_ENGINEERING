1fbadb5a128e60eb1462907a6ebc3bd1e2ef469a
BogdanBala112 <bogdan.balacescu@stud.ubbcluj.ro>
2025-03-24 19:25:27 +0200

    Delete exercise.py

--- exercise.py
+++
@@ -1,8 +0,0 @@
-def create_square(n):
-    for i in range(n):
-        if i == 0 or i == n - 1:
-            print('X ' * n)
-        else:
-            print('X ', ' ' * (n - 2), ' X')
-
-create_square(5)
